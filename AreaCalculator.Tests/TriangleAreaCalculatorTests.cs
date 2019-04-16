4ec852091a4105a8cb82409bbe9039ac914b707b
Andrew <nowosad@inbox.ru>
2019-04-16 10:18:10 +0400

    Добавлен тест для некорректных сторон (a+b<=c) и тест для корректных (a+b>c)

--- AreaCalculator.Tests/TriangleAreaCalculatorTests.cs
+++ AreaCalculator.Tests/TriangleAreaCalculatorTests.cs
@@ -17,6 +17,29 @@ namespace AreaCalculator.Tests
                 () => new TriangleAreaCalculator(a, b, c));
         }
 
+        [Theory]
+        [InlineData(1, 2, 4)]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 1, 1)]
+        [InlineData(2, 8, 2)]
+        [InlineData(10, 10, 35)]
+        public void CantCreateTriangleWithVeryLargeSide(double a, double b, double c)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new TriangleAreaCalculator(a, b, c));
+        }
+
+        [Theory]
+        [InlineData(2, 3, 4)]
+        [InlineData(3, 4, 5)]
+        [InlineData(3, 4, 6)]
+        [InlineData(7, 3, 5)]
+        [InlineData(9, 15, 7)]
+        public void CanCreateTriangleWithCorrectSides(double a, double b, double c)
+        {
+            var t = new TriangleAreaCalculator(a, b, c);
+        }
+
         [Fact]
         public void CalcAreaTest()
         {
