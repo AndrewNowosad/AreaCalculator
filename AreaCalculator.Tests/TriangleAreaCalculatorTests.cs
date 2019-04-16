5888a48580ea5085e78c8480dc37afadfc3223e6
Andrew <nowosad@inbox.ru>
2019-04-16 10:10:52 +0400

    Добавлен тест создания треугольника с неположительной стороной

--- AreaCalculator.Tests/TriangleAreaCalculatorTests.cs
+++ AreaCalculator.Tests/TriangleAreaCalculatorTests.cs
@@ -5,6 +5,18 @@ namespace AreaCalculator.Tests
 {
     public class TriangleAreaCalculatorTests
     {
+        [Theory]
+        [InlineData(-1, 1, 2)]
+        [InlineData(2, -1, 2)]
+        [InlineData(3, 2, -1)]
+        [InlineData(1, 0, 1)]
+        [InlineData(3, -2, 0)]
+        public void CantCreateTriangleWithNonPositiveSide(double a, double b, double c)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new TriangleAreaCalculator(a, b, c));
+        }
+
         [Fact]
         public void CalcAreaTest()
         {
