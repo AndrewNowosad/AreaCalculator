c0eea691fe78a100bdf897c4b08f2e85a1528b88
Andrew <nowosad@inbox.ru>
2019-04-16 10:56:57 +0400

    Добавлен класс для расчета площади круга и конструктор в нем

---
+++ AreaCalculator/CircleAreaCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AreaCalculator
+{
+    public class CircleAreaCalculator : IAreaCalculator
+    {
+        #region public properties
+        public double R { get; }
+        #endregion public properties
+
+        #region public ctor
+        public CircleAreaCalculator(double r)
+        {
+            if (r <= 0)
+                throw new ArgumentException(
+                    "The radius of the circle must be positive!");
+
+            R = r;
+        }
+        #endregion public ctor
+
+        public double CalcArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
