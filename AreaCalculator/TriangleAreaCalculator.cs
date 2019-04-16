82d3f84bab20bb58dab1204e360b55d91f28e109
Andrew <nowosad@inbox.ru>
2019-04-16 10:10:14 +0400

    Добавлен конструктор треугольника

--- AreaCalculator/TriangleAreaCalculator.cs
+++ AreaCalculator/TriangleAreaCalculator.cs
@@ -1,13 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace AreaCalculator
 {
     public class TriangleAreaCalculator : IAreaCalculator
     {
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+
+        public TriangleAreaCalculator(double a, double b, double c)
+        {
+            Regularize(ref a, ref b, ref c);
+
+            if (a <= 0)
+                throw new ArgumentException(
+                    "The sides of the triangle must be positive!");
+
+            if (a + b <= c)
+                throw new ArgumentException(
+                    "A triangle with specified sides does not exist!");
+
+            (A, B, C) = (a, b, c);
+        }
+
         public double CalcArea()
         {
             throw new NotImplementedException();
+        }
+
+        private void Regularize(ref double a, ref double b, ref double c)
+        {
+            Regularize(ref a, ref b);
+            Regularize(ref b, ref c);
+            Regularize(ref a, ref b);
+        }
+
+        private void Regularize(ref double a, ref double b)
+        {
+            if (a > b) (a, b) = (b, a);
         }
     }
 }
