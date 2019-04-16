2b404e518e1693d49503d4e1dea5f99c76676d63
Andrew <nowosad@inbox.ru>
2019-04-16 11:01:32 +0400

    Добавлены тесты конструктора вычислителя площади круга

---
+++ AreaCalculator.Tests/CircleAreaCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace AreaCalculator.Tests
+{
+    public class CircleAreaCalculatorTests
+    {
+        [Theory]
+        [InlineData(-2)]
+        [InlineData(-0.002)]
+        [InlineData(0)]
+        public void CantCreateCircleWithNonPositiveRadius(double r)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new CircleAreaCalculator(r));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5.3)]
+        [InlineData(17.001)]
+        public void CanCreateCircleWithCorrectRadius(double r)
+        {
+            var t = new CircleAreaCalculator(r);
+        }
+    }
+}
