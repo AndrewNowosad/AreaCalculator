509e4106b3284ca741c7b30ad194f0e942e624ee
Andrew <nowosad@inbox.ru>
2019-04-16 11:13:09 +0400

    Добавлен тестовый проект с примером использования библиотеки. Показано как вычислить площадь фигуры без знания типа фигуры

---
+++ AreaCalculator.Samples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AreaCalculator.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calcs = new IAreaCalculator[]
+            {
+                new TriangleAreaCalculator(3, 4, 5),
+                new CircleAreaCalculator(1),
+                new TriangleAreaCalculator(5, 5, 5),
+                new CircleAreaCalculator(3.8),
+                new TriangleAreaCalculator(7, 3, 5)
+            };
+
+            foreach (var calc in calcs)
+                Console.WriteLine(calc.CalcArea());
+
+            Console.ReadKey();
+        }
+    }
+}
